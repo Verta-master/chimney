36f628905ec709ed279539892e56ad23138c7e9b
Verta-master <vertyagina@gmail.com>
2019-12-05 13:19:39 +0600

    Update 2019-12-05T07:19:35.003Z

--- js/script.js
+++
@@ -1,1 +0,0 @@
-$(".menu__btn").click(function(){$(".menu").toggleClass("menu--opened"),$(".header").toggleClass("header--menu"),$(".menu__drop").slideToggle()});
--- js/tabs.js
+++
@@ -1,1 +0,0 @@
-$(".card__option").click(function(){$(this).next().slideToggle(),$(this).toggleClass("card__option--opened")}),$(".card__suboption").click(function(){$(this).next().slideToggle(),$(this).toggleClass("card__suboption--opened")});
